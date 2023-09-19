2aec45ce2549debcf777a923de8943841bf44a6e
Eriny <eriny.zgendy@gmail.com>
2023-09-18 18:14:33 -0700

    101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,47 +3,49 @@
 #include <stdlib.h>
 #include <time.h>
 #include "main.h"
-/**
- * checksum - executes checksum
- * @s: input char
- * Return: checksum
- */
-unsigned long checksum(char *s)
-{
-unsigned long sum = 0;
-while (*s != 0)
-{
-	sum += *s;
-	s++;
-}
-return (sum);
-}
-/**
- * main - prints password for crakme
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQSTUVWXYZ";
-	char s[33];
-	unsigned long sum;
-	int i, flag = 0;
 
-	srand(time(NULL));
-	while (flag == 0)
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		for (i = 0; i < 33; i++)
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			s[i] = alpha[rand() % (sizeof(alpha) - 1)];
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-		s[i] = '\0';
-		sum = checksum(s);
-		if (sum == 2772)
-		{
-			flag = 1;
-			printf("%s", s);
-		}
+		i++;
 	}
-return (0);
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
 
